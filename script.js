a284f947c8129963a6500f356b4d6b0d727fa979
Klaradol <145441269+Klaradol@users.noreply.github.com>
2023-10-16 15:04:51 +0200

    java

--- script.js
+++ script.js
@@ -9,7 +9,25 @@ function myFunction(){
 
    const userFirstName=document.getElementById("firstName").value
    console.log(userFirstName)
-   const userLirstName=document.getElementById("lirstName").value
+
+
+   const userLastName=document.getElementById("lastName").value
+
    const fullName= userFirstName + " " + userLastName
+
    document.getElementById("fullName").value=userFullNAme
 } 
+function addition(){
+    const inputFirstNumber = parseInt(document.getElementById("firstNumber").value)
+    const inputSecondNumber = parseInt(document.getElementById("secondNumber").value)
+
+
+    document.getElementById("fullName").value=additionResult
+}
+function subtraction(){
+    const inputFirstNumber = parseInt(document.getElementById("firstNumber").value)
+    const inputSecondNumber = parseInt(document.getElementById("secondNumber").value)
+
+    document.getElementById("fullName").value=additionResult
+
+}
